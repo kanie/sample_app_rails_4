8e5f04166fb08e35e8c85143f4d3aeb94808f6cf
蟹江 隼人 <shimamura@MacBook-Pro-4.local>
2017-11-30 12:08:26 +0900

    予定日を自動で取得する

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,11 @@
 class Project < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
+
+  def term
+    self.start_date.business_dates(dailies)
+  end
+
+  def dailies
+    tasks.map(&:dailies).flatten.map(&:the_date).uniq.size
+  end
 end
